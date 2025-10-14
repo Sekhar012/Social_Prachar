ffa2fafbcc24d9158043314935d4f4eb00a68dfd
Sekhar012 <rajasekhargondu999@gmail.com>
2025-10-14 10:08:39 +0530

    updated_vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // required for Vercel
+  base: './', // required for Vercel
 })
